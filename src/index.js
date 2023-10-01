68596fef84beed96f96faadfedca792801ce1d36
paoloBugarin <paolopaolobugarin@gmail.com>
2023-10-01 00:48:14 -0700

    updated theme

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { theme } from "./materialTheme";
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <App />
+      </ThemeProvider>
   </React.StrictMode>
 );
 
--- src/materialTheme.js
+++ src/materialTheme.js
@@ -1,1 +1,29 @@
 import { createTheme } from '@mui/material';
+
+
+export const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#565857',
+    },
+    secondary: {
+      main: '#455e58',
+    },
+    background: {
+      default: '#f1e9e7',
+      paper: '#d8cec2',
+    },
+    text: {
+      primary: '#565857',
+      secondary: 'rgba(86,88,87,0.75)',
+      disabled: 'rgba(86,88,87,0.5)',
+    },
+    info: {
+      main: '#cad9d2',
+    },
+    success: {
+      main: '#8aa48c',
+    },
+  },
+});

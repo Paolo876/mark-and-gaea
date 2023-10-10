d1aa9c9648b3624e74c36d3b3b8e18c89269491e
paoloBugarin <paolopaolobugarin@gmail.com>
2023-10-09 22:36:11 -0700

    updated landing page

--- src/pages/home/Landing.jsx
+++ src/pages/home/Landing.jsx
@@ -3,11 +3,23 @@ import { Box, Typography } from '@mui/material';
 import Image from 'mui-image';
 import and from "../../assets/images/and.svg"
 import flower from "../../assets/images/flower.svg"
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <Box sx={{height: "100vh"}}>
       <Box sx={{height: "100%", display: "flex", flexDirection: "column", alignItems: 'center', justifyContent: "center", gap: 8}}>
+        <Box sx={{display: "flex", justifyContent: "space-between", width: "100%"}}>
+          <Box>
+            <Link>Details</Link>
+          </Box>
+          <Box>
+            <Link>Maps</Link>
+          </Box>
+          <Box>
+            <Link>RSVP</Link>
+          </Box>
+        </Box>
         <Box mb={4}>
           <Typography sx={{letterSpacing: 7, fontSize: {xl: 20}, opacity: .8}}>THE WEDDING OF</Typography>
         </Box>

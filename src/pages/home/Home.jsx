c86998142e71c8c9a5b223e3384e4cc08d9a92db
paoloBugarin <paolopaolobugarin@gmail.com>
2023-10-10 22:59:59 -0700

    updated home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,3 +14,35 @@ const Home = () => {
 }
 
 export default Home
+
+/*
+
+I am not just me;
+I am you and you are me--
+We are. Hereafter.
+
+
+2. The Details
+
+Ceremony and Reception
+will be at The V Farm 
+[scan QR Code for google maps] 
+
+We request that you come in formal attire. 
+Ladies: Long dress
+Men: Long sleeves and black slacks
+[insert palette]
+
+
+ 
+3. RSVP
+We reserved __ seats for you.
+
+A little note on gifts
+Your love, laughter and company is all we wish for on our special day. 
+However, if you wish to delight us with a gift, we are registered at 
+[insert qr code ng gift registry]
+or just surprise us in your own way! 🫶🏼
+
+
+*/

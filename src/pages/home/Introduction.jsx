5f69ee822629f950b10c443f6277af8581e53910
paoloBugarin <paolopaolobugarin@gmail.com>
2023-10-11 00:17:36 -0700

    created introduction component

---
+++ src/pages/home/Introduction.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Typography, Container } from '@mui/material';
+
+const Introduction = () => {
+  return (
+    <Container>
+			<Box sx={{height: "100vh"}}>
+				
+			</Box>
+		</Container>
+  )
+}
+
+export default Introduction

4ad8b66935d9b7f14e1d955dc612166ebb8674a4
paoloBugarin <paolopaolobugarin@gmail.com>
2023-10-13 00:19:54 -0700

    updated ui

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { AppBar, Box, Link, Typography } from '@mui/material'
+import { AppBar, Box, Link, Typography, Container } from '@mui/material'
 const Appbar = () => {
   return (
-    <AppBar component="nav" sx={{background: "none", boxShadow: 0, display: "flex", flexDirection: "column"}}>
-      <Box>
-        <Typography color="text.primary">M|G</Typography>
-      </Box>
-      <Box>
-        <Link>Details</Link>
-        <Link>Map</Link>
-        <Link>RSVP</Link>
-      </Box>
-    </AppBar>  
+    <AppBar component="nav" sx={{background: "none", boxShadow: 0, py: 2}}>
+      <Container>
+        <Box sx={{display: "flex", flexDirection: "row", width: "100%", justifyContent: "space-between", alignItems: "center"}}>
+          <Box>
+            <Typography color="text.primary">M|G</Typography>
+          </Box>
+          <Box sx={{display: "flex", gap: 5}}>
+            <Link><Typography>Details</Typography></Link>
+            <Link><Typography>Map</Typography></Link>
+            <Link><Typography>RSVP</Typography></Link>
+          </Box>
+        </Box>
+      </Container>
+    </AppBar> 
   )
 }
 
--- src/pages/home/Introduction.jsx
+++ src/pages/home/Introduction.jsx
@@ -8,14 +8,20 @@ import Appbar from '../../components/Appbar';
 const Introduction = () => {
   return (
     <Container>
-      <Box>
-        <Appbar/>
+      <Appbar/>
+      <Box sx={{zIndex: 1, height: "100vh", display: "flex", flexDirection: "column", alignItems: 'center', justifyContent: "center", gap: 8}}>
+        <Box>
+          <Typography variant="h3">MARK & GAEA</Typography>
+        </Box>
+        <Box>
+          <Typography sx={{letterSpacing: 7, fontSize: {xl: 25}}}>DECEMBER  .  22  .  2023</Typography>
+        </Box>
       </Box>
 
       {/* background */}
-      <Box sx={{position: "absolute", height: "100vh", top:0, left: 0, width: "100%", opacity: .6, filter: "contrast(80%) brightness(95%)"}}>
+      <Box sx={{position: "absolute", height: "100vh", top:0, left: 0, width: "100%", opacity: .6, filter: "contrast(80%) brightness(95%)", zIndex: -1}}>
         <Box sx={{position: "absolute", bottom: 0, left: "50%", transform: "translateX(-50%)", width: "100%"}}>
-          <Image src={image_1} duration={1500} height="auto" fit="contain" width="auto" sx={{maxHeight: "75vh"}}/>
+          <Image src={image_1} duration={1500} height="auto" fit="contain" width="auto" sx={{maxHeight: "60vh"}}/>
         </Box>
         <Box 
           sx={{

1b6fbf7c6b70b06579842535eaebd793efc08413
paoloBugarin <paolopaolobugarin@gmail.com>
2023-10-14 23:55:13 -0700

    updated landingpage

--- src/pages/home/MainLanding.jsx
+++ src/pages/home/MainLanding.jsx
@@ -10,29 +10,36 @@ const MainLanding = () => {
   return (
     <Container maxWidth="xl">
       <Box sx={{height: "100vh", position: 'relative'}}>
-        <Grid container sx={{width: "100%"}}>
-          <Grid item xs={6} sx={{width: "100%", textAlign: "left"}}>
-            <Box mb={4}>
-              <Typography sx={{letterSpacing: 7, fontSize: {xl: 20}, opacity: .8}}>THE WEDDING OF</Typography>
+        <Grid container sx={{width: "100%", height: "100%"}}>
+          <Grid item xs={6} sx={{width: "100%", height: "100%", textAlign: "left", display: "flex", flexDirection: "column", justifyContent: "center", pb: 10}}>
+            <Box mb={15}>
+              <Typography sx={{letterSpacing: 7, fontSize: {xl: 23}, opacity: .8}}>THE WEDDING OF</Typography>
             </Box>
             <Box sx={{display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent:"left", gap: 2.5}}>
-              <Typography variant="h3" fontSize={65} letterSpacing={8}>MARK</Typography>
+              <Typography variant="h3" fontSize={70} letterSpacing={8}>MARK</Typography>
               <Box align="left" sx={{opacity: .75}}>
                 <Image src={and} alt="and" duration={0} height={50} width="auto" fit="scale-down"/>
               </Box>
-              <Typography variant="h3" fontSize={65} letterSpacing={8}>GAEA</Typography>
+              <Typography variant="h3" fontSize={70} letterSpacing={8}>GAEA</Typography>
             </Box>
-            <Box sx={{opacity: .75}}>
-              <Typography sx={{letterSpacing: 7, fontSize: {xl: 20}}}>DECEMBER  .  22  .  2023</Typography>
-
+            <Box sx={{opacity: .75}} mt={20}>
+              <Typography sx={{letterSpacing: 8, fontSize: {xl: 22}}}>DECEMBER  .  22  .  2023</Typography>
+              <Typography sx={{transform: "skewX(-12deg)", mt: 1, letterSpacing: 2, fontSize:{xl: 17}}}> at four o'clock in the afternoon</Typography>
+              <Typography sx={{letterSpacing: 7, fontSize:{xl: 17}, my: .25}}>THE  V  FARM</Typography>
+              <Typography sx={{transform: "skewX(-12deg)", letterSpacing: 2, fontSize:{xl: 17}}}> Tarlac, Philippines</Typography>
             </Box>
           </Grid>
-          <Grid item xs={6} sx={{width: "100%", background: 'black'}}>
-            <Image src={headerImage} alt="headerImage" duration={0} fit="scale-down"/>
+          <Grid item xs={6} sx={{width: "100%", background: 'black', display:"flex", justifyContent: "right", alignItems: "flex-end"}}>
+            <Box>
+              <Image src={headerImage} alt="headerImage" duration={0} fit="scale-down" sx={{maxHeight: "90vh",}}/>
+            </Box>
           </Grid>
         </Grid>
+      </Box>
 
-        {/* backdrop */}
+      {/* backdrop */}
+      <Box sx={{position: "absolute", zIndex: -1, height: "100vh", width: "100%", top:0, left: 0, overflowX: "hidden"}}>
+        <Box sx={{position: "absolute", top:0, left: "50%", width: "50vw", background: "black", height: "100%", zIndex: -1}}></Box>
       </Box>
     </Container>
   )

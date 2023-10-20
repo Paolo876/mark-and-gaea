50dceaddfcc437728c60e24850ab43b45a7b2513
paoloBugarin <paolopaolobugarin@gmail.com>
2023-10-19 22:35:38 -0700

    updated ui

--- src/pages/home/RsvpForm.jsx
+++ src/pages/home/RsvpForm.jsx
@@ -39,35 +39,35 @@ const inputStyles = {
 }
 
 const formItemContainer = {
-  mb: 7
+  mb: 6
 }
 
 const radioLabelStyles = {
   typography: {
-    fontSize: 18,
-    letterSpacing: 1.25,
+    fontSize: 20,
+    letterSpacing: 1.5,
     color: "text.secondary",
     textShadow: "1px 1px 3px rgba(15,15,15,.05)",
     pl: .5,
+    fontFamily: "Bodoni-Bold"
   }
 }
 
 const RsvpForm = React.forwardRef((props, ref) => {
   return (
     <Container>
-      <Box sx={{position: 'relative', mb: 20}}>
+      <Box sx={{position: 'relative', mb: 40}}>
         <Grid container>
           <Grid item xs={12} sx={{mb: 16, pt: 15}} ref={ref}>
             <Typography variant="h4" sx={{fontSize: {lg: 50}}}>be our guest</Typography>
           </Grid>
           <Grid item xs={12} sx={{textAlign: "left", mb: 1}}>
-            <Typography sx={{fontSize: 35, letterSpacing: 6, fontFamily: "Bodoni-Bold", opacity: .85}}>RSVP</Typography>
+            <Typography sx={{fontSize: 35, letterSpacing: 8, fontFamily: "Bodoni-Bold", opacity: .75}}>RSVP</Typography>
           </Grid>
-
           <Grid item xs={12}>
             <Grid container>
               <Grid item xs={7} sx={{textAlign: "left", mt: 1}}>
-                <Box component="form" sx={{display: "flex", flexDirection: "column", pr: 10, height: "100%", pb: 5}}>
+                <Box component="form" sx={{display: "flex", flexDirection: "column", pr: 10, height: "100%", pb: .5}}>
                   <Box sx={formItemContainer}>
                     <TextField 
                       variant="standard" 
@@ -79,7 +79,6 @@ const RsvpForm = React.forwardRef((props, ref) => {
                     />
                   </Box>
                   <Box sx={formItemContainer}>
-                    {/* <Typography sx={labelStyles.sx}>Would you be able to attend the occasion?</Typography> */}
                     <FormControl sx={{pl: .5}}>
                       <RadioGroup
                         aria-labelledby="demo-radio-buttons-group-label"
@@ -102,41 +101,48 @@ const RsvpForm = React.forwardRef((props, ref) => {
                     </FormControl>
                   </Box>
                   <Box sx={{...formItemContainer, display: "flex", justifyContent: "space-between", width: "100%"}}>
-                    <Typography sx={labelStyles.sx}>How many are in your party?</Typography>
-                    <TextField inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="standard"/>
+                    <Box>
+                      <Typography sx={labelStyles.sx}>How many are in your party?</Typography>
+                    </Box>
+                    <Box sx={{width: "20%"}}>
+                      <TextField inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="standard" sx={{width: "100%"}} InputProps={inputStyles}/>
+                    </Box>
                   </Box>
                   <Box sx={{...formItemContainer, display: "flex", justifyContent: "space-between", width: "100%"}}>
-                    <Typography sx={labelStyles.sx}>Contact Number</Typography>
-                    <TextField inputProps={{ inputMode: 'numeric', pattern: '[0-9]*'}} variant="standard" InputProps={{sx: {width: "100%"}}}/>
+                    <Box>
+                      <Typography sx={labelStyles.sx}>Contact Number</Typography>
+                    </Box>
+                    <Box sx={{width: "50%"}}>
+                      <TextField inputProps={{ inputMode: 'numeric', pattern: '[0-9]*'}} variant="standard" sx={{width: "100%"}} InputProps={inputStyles}/>
+                    </Box>
                   </Box>
                   <Box sx={formItemContainer}>
                     <Typography sx={labelStyles.sx}>
                       Any message or requests?
                       <Box component="small" sx={{opacity: .9, pl: 1, letterSpacing: 1.25, fontFamily: "Bodoni"}}>(optional)</Box>
                     </Typography>
-                    <TextField color='secondary' multiline rows={3} sx={{width: "100%"}} value="di pa tapos to ha. nabusy ako sa homeworks knina e, aadjust ko pa sizing ng inputs hehehe ----pao"/>
+                    <TextField color='secondary' multiline rows={3} sx={{width: "100%"}}/>
                   </Box>
                   <Box mt="auto">
-                    <Button variant="contained" color="success" sx={{color: "white", fontSize: 19, letterSpacing: 4, fontFamily: "Bodoni-Bold"}}>Submit</Button>
+                    <Button variant="contained" color="success" sx={{color: "white", fontSize: 19, letterSpacing: 4, fontFamily: "Bodoni-Bold"}} disabled>Submit</Button>
                   </Box>
                 </Box>
-
               </Grid>
               <Grid item xs={5} sx={{display: "flex", alignItems: "flex-start", flexDirection: "column"}}>
                 <Box>
                   <Image src={image} fit="scale-down" sx={{filter: "grayscale(50%)"}}/>
                 </Box>
-                <Box sx={{textAlign: "left", mt: 3}}>
-                  <Typography sx={{fontFamily: "Bodoni", fontSize: 20, letterSpacing: .25, textAlign: "left", lineHeight: 1.4, color: "secondary.main"}}>
-                    <Box sx={{height: "1px", width: 20, backgroundColor: "primary.main", display: "inline-block", transform: "translateY(-8px)", mr: .5, opacity: .35}}></Box>
-                    Your love, laughter, and company is all we wish for on our special day. However, if you wish to delight us with a gift, just surprise us in your own way.
-                  </Typography>
-                  {/* <Typography sx={descriptionStyles}>
-                    Your love, laughter, and company is all we wish for on our special day. However, if you wish to delight us with a gift, just surprise us in your own way.
-                  </Typography> */}
-                </Box>
               </Grid>
             </Grid>
+          </Grid>
+          <Grid item xs={6} sx={{mt: 35, mx: "auto"}}>
+            {/* <Typography sx={{fontSize: 25, letterSpacing: 8, fontFamily: "Bodoni-Bold", opacity: .75, mb: 5,}}>A LITTLE NOTE ON GIFTS</Typography> */}
+            <Typography variant="h4" sx={{fontSize: {lg: 35}, mb: 8, letterSpacing: "initial"}}>a little note on gifts</Typography>
+
+            <Typography sx={{fontFamily: "Bodoni", fontSize: 25, letterSpacing: .25, textAlign: "center", lineHeight: 1.4, color: "secondary.main",}}>
+              <Box sx={{height: "1px", width: 20, backgroundColor: "primary.main", display: "inline-block", transform: "translateY(-8px)", mr: .5, opacity: .35}}></Box>
+              Your love, laughter, and company is all we wish for on our special day. However, if you wish to delight us with a gift, just surprise us in your own way.
+            </Typography>
           </Grid>
         </Grid>
       </Box>

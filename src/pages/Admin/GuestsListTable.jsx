626a5a1048ca1488f81e44ef059ae6a8701ebde0
paoloBugarin <paolopaolobugarin@gmail.com>
2023-10-24 22:25:11 -0700

    updated guests list table

--- src/pages/Admin/GuestsListTable.jsx
+++ src/pages/Admin/GuestsListTable.jsx
@@ -1,27 +1,46 @@
 import * as React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Box, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Box, Paper, IconButton, Button } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
+import MessageIcon from '@mui/icons-material/Message';
 
-// import Paper from '@mui/material/Paper ';
 
-function createData(name, isAttending, phone, message) {
-  return { name, isAttending, phone, message };
+const headerStyles = {
+	fontFamily: "Bodoni-bold",
+	textTransform: "uppercase",
+	letterSpacing: 3,
+	fontSize: 17,
 }
 
-const rows = [
-  createData('Paolo', true, "09053122966", "Hello"),
+const bodyStyles = {
+	fontFamily: "Bodoni",
+	letterSpacing: 1.25,
+	fontSize: 15
+}
 
-];
+function createData(name, isAttending, phone, message, createdAt) {
+  return { name, isAttending, phone, message, createdAt };
+}
 
+
+const mockDate = new Date().toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'})
 export default function BasicTable() {
+	const rows = [
+		createData('Paolo', true, "09053122966", "Hello", mockDate),
+		createData('Paolo2', false, "09053122966", "", mockDate),
+		createData('Paolo Paolo Paolo Paolo Paolo', false, "09053122966", "", mockDate),
+	
+	];
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: "fit-content" }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">isAttending</TableCell>
-            <TableCell align="right">Contact Number</TableCell>
-            <TableCell align="right">Message</TableCell>
+            <TableCell sx={headerStyles}>Name</TableCell>
+            <TableCell sx={headerStyles} align="center">Attending</TableCell>
+            <TableCell sx={headerStyles} align="center">Contact #</TableCell>
+            <TableCell sx={headerStyles} align="center">Message</TableCell>
+            <TableCell sx={headerStyles} align="right">Date</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -30,12 +49,14 @@ export default function BasicTable() {
               key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
+              <TableCell sx={bodyStyles} component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.isAttending}yes</TableCell>
-              <TableCell align="right">{row.phone}</TableCell>
-              <TableCell align="right">{row.message}</TableCell>
+              <TableCell align="center">{row.isAttending ? <CheckCircleIcon color="success"/> : <CancelIcon color="warning"/>}</TableCell>
+              <TableCell sx={bodyStyles} align="center">{row.phone}</TableCell>
+              <TableCell align="center">{row.message ? <Button startIcon={<MessageIcon/>} size="small" variant="contained" color="secondary" sx={{letterSpacing: 1}}>View Message</Button> : ""}</TableCell>
+							<TableCell sx={bodyStyles} align="right">{row.createdAt}</TableCell>
+
             </TableRow>
           ))}
         </TableBody>
